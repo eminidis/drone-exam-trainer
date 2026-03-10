599b5ea36b4ef9d955d97fd1a4f117b1f6c50b29
eminidis <k.eminides@Hotmail.com>
2026-03-10 14:46:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let questions = [];
 let currentQuestion = 0;
+let answered = false;
 
 async function loadQuestions(){
 const response = await fetch("questions.json");
@@ -13,18 +14,21 @@ showQuestion();
 
 function showQuestion(){
 
-if(currentQuestion >= questions.length){
-alert("Τέλος ερωτήσεων");
-return;
-}
+answered = false;
 
 const q = questions[currentQuestion];
 
-let html = "<h2>"+q.question+"</h2>";
+let html = `
+<h2>Ερώτηση ${currentQuestion+1}</h2>
+<p style="font-size:22px">${q.question}</p>
+`;
 
 q.answers.forEach((a,i)=>{
-html += `<button onclick="checkAnswer(${i})">${a}</button><br><br>`;
+html += `<button onclick="checkAnswer(${i})" id="a${i}" style="display:block;margin:10px auto;padding:15px;width:300px;font-size:18px">${a}</button>`;
 });
+
+html += `<div id="result" style="margin-top:20px;font-size:22px"></div>`;
+html += `<button onclick="nextQuestion()" style="margin-top:20px;padding:10px 20px">Επόμενη ερώτηση</button>`;
 
 document.getElementById("quiz").innerHTML = html;
 
@@ -32,16 +36,41 @@ document.getElementById("quiz").innerHTML = html;
 
 function checkAnswer(i){
 
+if(answered) return;
+
+answered = true;
+
 const correct = questions[currentQuestion].correct;
 
 if(i === correct){
-alert("Σωστό");
+
+document.getElementById("a"+i).style.background="green";
+document.getElementById("result").innerHTML="✔ Σωστό";
+
 }else{
-alert("Λάθος");
+
+document.getElementById("a"+i).style.background="red";
+document.getElementById("a"+correct).style.background="green";
+
+document.getElementById("result").innerHTML="❌ Λάθος";
+
 }
 
+}
+
+function nextQuestion(){
+
 currentQuestion++;
+
+if(currentQuestion >= questions.length){
+
+document.getElementById("quiz").innerHTML="<h2>Τέλος Study Mode</h2>";
+return;
+
+}
+
 showQuestion();
+
 }
 
 loadQuestions();
