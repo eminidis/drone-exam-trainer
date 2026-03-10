210cdd18cc75064d2b08b32b89cf672e6ff9a6da
eminidis <k.eminides@Hotmail.com>
2026-03-10 14:34:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
-function startStudy(){
-alert("Study Mode λειτουργεί");
+let questions = [];
+let currentQuestion = 0;
+
+async function loadQuestions(){
+const response = await fetch("questions.json");
+questions = await response.json();
 }
 
-function startExam(){
-alert("Exam Mode λειτουργεί");
+function startStudy(){
+currentQuestion = 0;
+showQuestion();
 }
+
+function showQuestion(){
+
+if(currentQuestion >= questions.length){
+alert("Τέλος ερωτήσεων");
+return;
+}
+
+const q = questions[currentQuestion];
+
+let html = "<h2>"+q.question+"</h2>";
+
+q.answers.forEach((a,i)=>{
+html += `<button onclick="checkAnswer(${i})">${a}</button><br><br>`;
+});
+
+document.getElementById("quiz").innerHTML = html;
+
+}
+
+function checkAnswer(i){
+
+const correct = questions[currentQuestion].correct;
+
+if(i === correct){
+alert("Σωστό");
+}else{
+alert("Λάθος");
+}
+
+currentQuestion++;
+showQuestion();
+}
+
+loadQuestions();
