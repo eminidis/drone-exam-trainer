235c9d19b853d233c53183d2a8d6a9355a8434f3
eminidis <k.eminides@Hotmail.com>
2026-03-10 14:18:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-let questions = [
-{
-question: "Test ερώτηση Drone Exam Trainer",
-answers: ["Απάντηση Α","Απάντηση Β","Απάντηση Γ","Απάντηση Δ"],
-correct: 0
-}
-];
-
 function startStudy(){
-alert("Study Mode ξεκίνησε");
+alert("Study Mode λειτουργεί");
 }
 
 function startExam(){
-alert("Exam Mode ξεκίνησε");
+alert("Exam Mode λειτουργεί");
 }
